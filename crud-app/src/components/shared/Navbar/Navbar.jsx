4bff54707cccd477f9601538d41c3bbdec9edaa5
MarzukaaZaki <zakimarzuka@gmail.com>
2023-12-28 12:28:27 +0600

    Added a navbar

---
+++ crud-app/src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logoImg from '../../../assets/logo/logo.png'
+import userImg from '../../../assets/user/user1.jpg'
+import './Navbar.css'
+import { FaCaretDown } from "react-icons/fa";
+const Navbar = () => {
+    return (
+        <nav className="p-4 flex items-center justify-between shadow-sm">
+            <div className="flex items-center">
+                
+                <img src={logoImg} alt="Logo" className="logo-img mr-4" />
+
+            </div>
+            <div className="px-2 flex items-center">
+                <div className='text-sm px-2'>
+                 <p> <span className='font-semibold'>Free Trial </span>| 2days left</p>   
+                 <p className='text-primary'> Extend free trial</p>
+                </div>
+                
+                
+                <img src={userImg} alt="Rounded Image" className="profile-img rounded-full" />
+                <FaCaretDown/>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- crud-app/src/pages/Home/Home.jsx
+++ crud-app/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../../components/shared/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
-            This is home
+            <Navbar/>
         </div>
     );
 };
