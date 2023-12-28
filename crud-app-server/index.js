3b29e28081d024963f9a351cb68203b22acfdad7
MarzukaaZaki <zakimarzuka@gmail.com>
2023-12-28 10:29:58 +0600

    Set up project and connected database

---
+++ crud-app-server/index.js
@@ -0,0 +1,62 @@
+// Import express from nodejs 
+const express = require('express');
+
+// Creating an instance of the express application
+const app = express();
+
+// Environment Variables
+require('dotenv').config();
+
+// Middleware
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json())
+
+// Defines port in which the server is to be launched
+// During deployment it will assign any of the available ports to our server.
+const port = process.env.PORT || 5000;
+
+
+
+// Connect Mongodb Database
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.VITE_DB_USER}:${process.env.VITE_DB_PASSWORD}@cluster0.10dhryt.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // API Endpoints
+        // Defining a URL and the response the users will get when they go to that url.
+        app.get('/', (request, response) => {
+            response.send('Hello from my projects server')
+        })
+
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+// Set the port at which the server should listen for incoming requests
+app.listen(port, () => {
+    console.log(`My projects server is running on port ${port}`);
+})
